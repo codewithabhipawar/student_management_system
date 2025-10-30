e64c81e39cce825b4598194eddfee09100bf6e48
abhi_pawar <ap7218362896@gmail.com>
2025-10-30 21:13:13 +0530

    modify controller

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,10 +49,12 @@ public class StudentController {
 	}
 
 	@GetMapping("/stud")
-	public String getStudents()
-	{
-		return "student.jsp";
-	}
+    public String showStudents(Model model) {
+        // Fetch data from database
+        model.addAttribute("student", stdService.getStudents());
+        // Return view name (students.jsp)
+        return "student";
+    }
 	
 	//post mapping
 	@PostMapping("/students")
